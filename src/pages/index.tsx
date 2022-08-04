d67fd53d19f4ba722aa603e83884bc268b48b353
kazijamal <jamalkazi02@gmail.com>
2022-08-04 16:43:38 -0400

    Use getServerSideProps on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
-import type { NextPage } from 'next';
+import type {
+  NextPage,
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+} from 'next';
 import Link from 'next/link';
 import Head from 'next/head';
-import { useSession } from 'next-auth/react';
-import { MoonLoader } from 'react-spinners';
+import { unstable_getServerSession as getServerSession } from 'next-auth';
+import { authOptions as nextAuthOptions } from './api/auth/[...nextauth]';
+import { Key } from 'react';
 
 type OutfitCardProps = {
   image: string;
@@ -10,48 +15,10 @@ type OutfitCardProps = {
   description: string;
 };
 
-const Home: NextPage = () => {
-  const { data: session, status } = useSession();
-
-  const recentOutfits = [
-    {
-      image: 'https://cdn.mos.cms.futurecdn.net/giaJ8MhXBN5YjDYKa4FbWH.jpg',
-      name: 'Tim Henson',
-      description: 'What jacket is he wearing?',
-    },
-    {
-      image:
-        'https://www.highsnobiety.com/static-assets/thumbor/wlnnmLyG7pzVjZVkTa3bx961cT8=/1600x1975/www.highsnobiety.com/static-assets/wp-content/uploads/2022/05/22222248/kanye-west-balenciaga-spring-2023-boots-outfit.jpg',
-      name: 'Kanye West',
-      description: 'What boots is he wearing?',
-    },
-    {
-      image:
-        'https://images.complex.com/complex/images/c_fill,dpr_auto,f_auto,q_auto,w_1400/fl_lossy,pg_1/p3rruj3otb7unklgckyp/best-tyler-the-creator-outfits-british-fashion-awards-2019?fimg-client-default',
-      name: 'Tyler the Creator',
-      description: 'What hat is he wearing?',
-    },
-  ];
-
-  if (status === 'loading') {
-    return (
-      <>
-        <Head>
-          <title>FitDetector</title>
-          <meta
-            name='description'
-            content='Find out what clothes your favorite celebrities are wearing'
-          />
-          <link rel='icon' href='/favicon.ico' />
-        </Head>
-
-        <main className='container mx-auto flex flex-col items-center justify-center min-h-screen p-4'>
-          <MoonLoader />
-        </main>
-      </>
-    );
-  }
-
+const Home: NextPage = ({
+  nextAuthSession,
+  recentOutfits,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
     <>
       <Head>
@@ -70,14 +37,7 @@ const Home: NextPage = () => {
         <p className='text-lg text-gray-600 m-2 text-center'>
           Find out what clothes your favorite celebrities are wearing
         </p>
-        {status === 'unauthenticated' && (
-          <Link href='/api/auth/signin'>
-            <a className='m-1 bg-purple-400 hover:bg-purple-600 text-white py-2 px-4 rounded'>
-              Sign in to submit a photo
-            </a>
-          </Link>
-        )}
-        {status === 'authenticated' && (
+        {nextAuthSession ? (
           <div className='flex'>
             <Link href='#'>
               <a className='m-1 bg-purple-400 hover:bg-purple-600 text-white py-2 px-4 rounded'>
@@ -90,6 +50,12 @@ const Home: NextPage = () => {
               </a>
             </Link>
           </div>
+        ) : (
+          <Link href='/api/auth/signin'>
+            <a className='m-1 bg-purple-400 hover:bg-purple-600 text-white py-2 px-4 rounded'>
+              Sign in to submit a photo
+            </a>
+          </Link>
         )}
         <form className='w-full max-w-md m-3'>
           <label className='mb-2 text-sm font-medium text-gray-900 sr-only'>
@@ -131,14 +97,19 @@ const Home: NextPage = () => {
           Recently submitted outfit photos
         </h2>
         <div className='m-2 flex gap-3 flex-col items-center'>
-          {recentOutfits.map((outfit, index) => (
-            <OutfitCard
-              image={outfit.image}
-              name={outfit.name}
-              description={outfit.description}
-              key={index}
-            />
-          ))}
+          {recentOutfits.map(
+            (
+              outfit: { image: string; name: string; description: string },
+              index: Key | null | undefined
+            ) => (
+              <OutfitCard
+                image={outfit.image}
+                name={outfit.name}
+                description={outfit.description}
+                key={index}
+              />
+            )
+          )}
         </div>
       </main>
     </>
@@ -165,3 +136,35 @@ const OutfitCard = ({ image, name, description }: OutfitCardProps) => {
 };
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const nextAuthSession = await getServerSession(
+    context.req,
+    context.res,
+    nextAuthOptions
+  );
+
+  const recentOutfits = [
+    {
+      image: 'https://cdn.mos.cms.futurecdn.net/giaJ8MhXBN5YjDYKa4FbWH.jpg',
+      name: 'Tim Henson',
+      description: 'What jacket is he wearing?',
+    },
+    {
+      image:
+        'https://www.highsnobiety.com/static-assets/thumbor/wlnnmLyG7pzVjZVkTa3bx961cT8=/1600x1975/www.highsnobiety.com/static-assets/wp-content/uploads/2022/05/22222248/kanye-west-balenciaga-spring-2023-boots-outfit.jpg',
+      name: 'Kanye West',
+      description: 'What boots is he wearing?',
+    },
+    {
+      image:
+        'https://images.complex.com/complex/images/c_fill,dpr_auto,f_auto,q_auto,w_1400/fl_lossy,pg_1/p3rruj3otb7unklgckyp/best-tyler-the-creator-outfits-british-fashion-awards-2019?fimg-client-default',
+      name: 'Tyler the Creator',
+      description: 'What hat is he wearing?',
+    },
+  ];
+
+  return {
+    props: { nextAuthSession, recentOutfits },
+  };
+};
